1dd1e1c0d197ecacdf8ff72a9e60e200133af386
Mitchell Hashimoto <andere@uchicago.edu>
2024-03-03 20:03:48 +0100

    adding chroma database /data for easy replicability

--- embed.py
+++ embed.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import chromadb
 from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import PyPDFLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import TextLoader
@@ -13,16 +14,16 @@ model_id = "gpt-3.5-turbo"
 llm = ChatOpenAI(model_name=model_id)
 
 # txt
-raw_documents = TextLoader('rawdata/catalog_data.txt').load()
+raw_documents = TextLoader('rawdata/data_majors.txt').load()
 text_splitter = CharacterTextSplitter(chunk_size=700, chunk_overlap=80)
-documents = text_splitter.split_documents(raw_documents)
+docs = text_splitter.split_documents(raw_documents)
 
 # pdf
-# loader = PyPDFLoader("data/document.pdf")
+# loader = PyPDFLoader("rawdata/.pdf")
 # docs = loader.load_and_split()
 
 chroma_db = Chroma.from_documents(
-    documents=documents, 
+    documents=docs, 
     embedding=OpenAIEmbeddings(), 
     persist_directory="data", 
     collection_name="catalog_db_pdf"
