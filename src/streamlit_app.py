9878c989b59de0114bfad772f60c2b5d6b733a71
Emilio Andere <emilioandere@Emi.local>
2024-03-10 15:06:04 +0100

    fix path pheonix logo error

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -26,7 +26,7 @@ st.set_page_config(
 col1, col2 = st.columns([1, 2])
 
 with col1:
-    st.image("resources/pheonixlogo.png", width=190)
+    st.image("../resources/pheonixlogo.png", width=190)
 
 with col2:
     st.subheader("Pheonix AI")
